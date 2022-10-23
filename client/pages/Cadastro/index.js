4576b2b4514fc908adb3a7328d36111b183dcfeb
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-10-23 12:30:27 -0300

    page-cadastro

---
+++ client/pages/Cadastro/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cadastro from '../../components/Cadastro';
+import Img from '../../components/imagens/loginIgm.js'
+
+
+
+
+function index(props)  {
+  return (
+   <>
+   <Img />
+   <Cadastro />
+   </>
+  ) 
+}
+
+export default index
